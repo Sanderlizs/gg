9b2c5386763d2b73716bae3186a9f136503db6d6
sander790723@gmail.com <sander790723@gmail.com>
2018-10-11 10:02:46 +0800

    release v1.0.0

--- src/sander/js/li/A.java
+++ src/sander/js/li/A.java
@@ -1,10 +1,15 @@
 package sander.js.li;
 
 public class A {
+  /**
+   * release v1.0.0
+   * 
+   * @param args
+   */
 
   public static void main(String[] args) {
     System.out.println("Test A process");
-  
+
   }
 
   private void theOne() {
