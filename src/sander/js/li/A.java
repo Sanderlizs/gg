28a60fe09fc06aff26c007901aa0af59a0d5dd71
sander790723@gmail.com <sander790723@gmail.com>
2018-10-11 09:28:33 +0800

    create

---
+++ src/sander/js/li/A.java
@@ -0,0 +1,10 @@
+package sander.js.li;
+
+public class A {
+
+  public static void main(String[] args) {
+    System.out.println("Test A process");
+
+  }
+
+}
---
+++ src/sander/js/li/B.java
@@ -0,0 +1,10 @@
+package sander.js.li;
+
+public class B {
+
+  public static void main(String[] args) {
+    System.out.println("Test B process by release version 0.1");
+
+  }
+
+}
