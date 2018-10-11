de228758d54d9cec01d13b1ab6ac0bdadf6b1824
sander790723@gmail.com <sander790723@gmail.com>
2018-10-11 09:30:27 +0800

    add new feature

--- src/sander/js/li/A.java
+++ src/sander/js/li/A.java
@@ -4,7 +4,10 @@ public class A {
 
   public static void main(String[] args) {
     System.out.println("Test A process");
-
+  
   }
 
+  private void theOne() {
+    System.out.println("add function 1");
+  }
 }
