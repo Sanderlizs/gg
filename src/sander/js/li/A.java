31c471d5bcac97ddf9c9d7dc97c2a1e4b771f435
sander790723@gmail.com <sander790723@gmail.com>
2018-10-11 11:26:18 +0800

    fix

--- src/sander/js/li/A.java
+++ src/sander/js/li/A.java
@@ -9,7 +9,7 @@ public class A {
 
   public static void main(String[] args) {
     System.out.println("Test A process");
-
+    System.out.println("release v1.0.0");
   }
 
   private void theOne() {
